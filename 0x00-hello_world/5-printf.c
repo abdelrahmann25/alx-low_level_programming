d52831f0eafc5e80ae731f2262583727416b4bd8
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-20 11:13:06 -0800

    Used printf + new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printf a string
+ *
+ * Return: Always 0 (Succeeded)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
