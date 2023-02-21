a0608adf5fb5925f3a2e0680313c3082b6feaa95
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-20 16:02:55 -0800

    Print 0 to 9 by ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always  0 (succeed)
+ */
+int main(void)
+{
+	char c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
