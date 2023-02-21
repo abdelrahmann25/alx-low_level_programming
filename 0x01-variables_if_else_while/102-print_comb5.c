f3b605bbb9daed4a4133c417bf4e842ea6cf0f7f
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-20 18:24:24 -0800

    Printing 00 00 to 99 99 without repeated combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Printing 00 00 to 99 99 without repeated combination
+ *
+ * Return: 0 (succeed)
+*/
+int main(void)
+{
+	int f, s, t, fo;
+
+	for (f = 48; f <= 57; f++)
+	{
+		for (s = 48; s <= 57; s++)
+		{
+			for (t = 48; t <= 57; t++)
+			{
+				for (fo = 48; fo <= 57; fo++)
+				{
+					if (s < f || fo < t)
+						continue;
+					if (s == f && fo == t)
+						continue;
+					putchar(f);
+					putchar(s);
+					putchar(' ');
+					putchar(t);
+					putchar(fo);
+					if (f == 57 && s == 56 && t == 57 && fo == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
