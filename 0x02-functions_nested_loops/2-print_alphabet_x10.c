7b809722493ed6c04de64cb4bab44affb5325f31
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-25 02:05:11 -0800

    3rd task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - prant all alphabets from a to z 10 times
+ * print_alphabet_x10 - prant all alphabets from a to z 10 times
  */
 void print_alphabet_x10(void)
 {
