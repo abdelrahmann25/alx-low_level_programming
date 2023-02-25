7b3fca0754cd5796f313a985f5fac18ff975a2cd
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-25 08:54:45 -0800

    3rd advanced task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,12 +11,13 @@ int main(void)
 	long int i = 1, j = 2, tmp = 0;
 
 	printf("%ld, %ld, ", i, j);
-	for (counter = 3; counter < 50; counter++)
+	for (counter = 3; counter <= 50; counter++)
 	{
 		tmp = i + j;
 		printf("%ld, ", tmp);
 		i = j;
 		j = tmp;
 	}
+	printf("\n");
 	return (0);
 }
