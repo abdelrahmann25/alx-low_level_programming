170da84580cc7e965a1a94d3fb099b4f12e3ebe5
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-25 05:54:51 -0800

    11th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - summing two values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: result of a + b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
