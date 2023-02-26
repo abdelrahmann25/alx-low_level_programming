5ba50e93fd64982a03383792647d0d91fcc3471c
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-26 04:08:12 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print Fizz Buzz test
+ *
+ * Return: 0 Succeded
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBUZZ");
+			printf(" ");
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+			continue;
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+			continue;
+		}
+		printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
