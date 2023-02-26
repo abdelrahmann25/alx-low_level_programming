2fa340e8f95c267d14bd61ca6363032cb8e1da8d
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-02-26 04:49:47 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
-			continue;
 		}
-		if (i % 5 == 0)
-		{
-			printf("Buzz");
-			printf(" ");
-			continue;
-		}
-		if (i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
-			continue;
 		}
-		printf("%d", i);
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
 		if (i != 100)
-			printf(" ");
+		{
+			putchar(' ');
+		}
 	}
 	printf("\n");
 	return (0);
