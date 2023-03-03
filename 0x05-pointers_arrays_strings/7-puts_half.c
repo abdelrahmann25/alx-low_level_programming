59fd0d43caa885b6eccb22e0811357e72197b4bf
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-03-03 03:52:46 -0800

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 		len++;
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
 			_putchar(str[i]);
 	}
 	else if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; i <= len; i++)
+		for (i = (len + 1) / 2; i < len; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
