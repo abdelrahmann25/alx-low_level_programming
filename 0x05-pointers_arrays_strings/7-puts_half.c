25b417f56f7bb2f75b849c903d1ffdc77db5004d
abdelrahmann25 <abdelrahmann25@yahoo.com>
2023-03-02 19:42:06 -0800

    print second half of string passed by ref

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts_half - print second half of a string passed by ref
+ * @str: the string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	for (i = len / 2; i <= len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
